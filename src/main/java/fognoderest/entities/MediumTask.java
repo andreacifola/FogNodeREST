aef347968914609a8eb66db7b659f5b9d342bc3e
Andrea Ranfone <ranfone.andrea@gmail.com>
2018-09-20 11:18:40 +0200

    aggiunti servizi rest

---
+++ src/main/java/fognoderest/entities/MediumTask.java
@@ -0,0 +1,22 @@
+package fognoderest.entities;
+
+public class MediumTask extends Task {
+    private int number;
+    private int time;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+}
