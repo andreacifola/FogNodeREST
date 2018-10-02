a2a15cf8f72709676a4e43cec583388f71f96a0b
Andrea Cifola <cifola.andrea@gmail.com>
2018-10-02 17:45:42 +0200

    Completato il mantenimento dello stato per i medium task

--- src/main/java/fognoderest/handler/GetStateHandler.java
+++ src/main/java/fognoderest/handler/GetStateHandler.java
@@ -1,7 +1,9 @@
 package fognoderest.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import fognoderest.entities.MediumTaskState;
 import fognoderest.utils.SendPostRequest;
+
 import java.io.IOException;
 
 
@@ -12,7 +14,7 @@ public class GetStateHandler {
     public void sendMediumTaskState(Integer state, Long currentTime, Integer taskId) throws IOException {
 
         String payload = mediumTaskStateToJson(taskId, state, currentTime);
-        String requestUrl="http://localhost:8080/state/mediumTask";
+        String requestUrl = "http://localhost:8080/mediumTaskState";
         sendPostRequestForTaskState(requestUrl, payload);
 
     }
@@ -23,46 +25,9 @@ public class GetStateHandler {
         return payload;
     }
 
-    private void sendPostRequestForTaskState(String requestUrl, String payload) throws IOException {
+    private MediumTaskState sendPostRequestForTaskState(String requestUrl, String payload) throws IOException {
         SendPostRequest sendPostRequest = new SendPostRequest();
         StringBuilder jsonString = sendPostRequest.postRequest(requestUrl, payload);
-        return;
-    }
-
-
-    private class MediumTaskState {
-        private Integer taskId;
-        private Integer state;
-        private Integer currentTime;
-
-        public MediumTaskState(Integer taskId, Integer state, Integer currentTime) {
-            this.taskId = taskId;
-            this.state = state;
-            this.currentTime = currentTime;
-        }
-
-        public Integer getTaskId() {
-            return taskId;
-        }
-
-        public void setTaskId(Integer taskId) {
-            this.taskId = taskId;
-        }
-
-        public Integer getState() {
-            return state;
-        }
-
-        public void setState(Integer state) {
-            this.state = state;
-        }
-
-        public Integer getCurrentTime() {
-            return currentTime;
-        }
-
-        public void setCurrentTime(Integer currentTime) {
-            this.currentTime = currentTime;
-        }
+        return mapper.readValue(jsonString.toString(), MediumTaskState.class);
     }
 }
